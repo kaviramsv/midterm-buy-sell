39fbf6475497950094c7077a8005f8776fa038bf
Dennis Law <denn15_law@live.ca>
2021-11-28 21:13:14 +0000

    removed unused code/comments

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,8 @@
-/*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
+//Require Express as Middleware
 const express = require("express");
 const router = express.Router();
+
+//Call item-query to use as functions
 const itemFnc = require("../db/item-query");
 
 router.get("/", (req, res) => {
@@ -16,19 +12,3 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
-
-// module.exports = (db) => {
-//   router.get("/", (req, res) => {
-//     let query = `SELECT * FROM items`;
-//     console.log(query);
-//     db.query(query)
-//       .then((data) => {
-//         const items = data.rows;
-//         res.json({ items });
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-//   return router;
-// };
--- server.js
+++ server.js
@@ -3,44 +3,20 @@ require("dotenv").config();
 
 // Web server config
 const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-// PG database client/connection setup
-// const { Pool } = require("pg");
-// const dbParams = require("./lib/db.js");
-// const db = new Pool(dbParams);
-// db.connect();
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// 'dev' = red - server error codes, yellow - client error codes, cyan for redirection codes, uncolored - other
 app.use(morgan("dev"));
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   "/styles",
-//   sassMiddleware({
-//     source: __dirname + "/styles",
-//     destination: __dirname + "/public/styles",
-//     isSass: false, // false => scss, true => sass
-//   })
-// );
-
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
 const itemsRoutes = require("./routes/items");
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// app.use("/api/users", usersRoutes);
 app.use("/items", itemsRoutes);
 
 //get and post for login
