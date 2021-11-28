aa0b2b2a14d47fe0d7d559b5f4fb1378ad2bf877
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-28 14:18:24 +0000

    ADD favourites seeds

---
+++ db/seeds/04_favourites.sql
@@ -0,0 +1,4 @@
+INSERT INTO favourites(item_id,user_id) VALUES (1,2);
+INSERT INTO favourites(item_id,user_id) VALUES (2,3);
+INSERT INTO favourites(item_id,user_id) VALUES (3,2);
+INSERT INTO favourites(item_id,user_id) VALUES (4,1);
