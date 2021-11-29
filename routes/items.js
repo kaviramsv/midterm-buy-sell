948adf29f3e847864b436e48196c2c12cc4e8ca8
Dennis Law <denn15_law@live.ca>
2021-11-29 03:16:51 +0000

    write get method to render ejs passing in queried items

--- routes/items.js
+++ routes/items.js
@@ -5,10 +5,18 @@ const router = express.Router();
 //Call item-query to use as functions
 const itemFnc = require("../db/item-query");
 
-router.get("/", (req, res) => {
-  itemFnc.getItems().then((items) => {
-    res.json(items);
+router.get("/user/:id", (req, res) => {
+  itemFnc.getItemsByUser(req.params.id).then((items) => {
+    const templateVars = {
+      items: items,
+    };
+    console.log(templateVars);
+    res.render("my-items", templateVars);
   });
+});
+
+router.get("/", (req, res) => {
+  res.render("my-items");
 });
 
 module.exports = router;
