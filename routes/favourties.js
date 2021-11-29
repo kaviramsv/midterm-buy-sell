44a4ab0b0a27d10f15c19ab596f1c79bf81ad4af
Dennis Law <denn15_law@live.ca>
2021-11-29 18:21:27 +0000

    add path /favourites/:id to render page

---
+++ routes/favourties.js
@@ -0,0 +1,22 @@
+//Require Express as middleware
+const express = require("express");
+const router = express.Router();
+
+//Call item-query.js to use functions
+const itemsFnc = require("../db/item-query");
+
+router.get("/:id", (req, res) => {
+  itemsFnc
+    .getFavItemsByUser(req.params.id)
+    .then((items) => {
+      const templateVars = {
+        favItems: items,
+      };
+      res.render("favourites", templateVars);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+module.exports = router;
